5456ea9eeb1e76b2f4c54f51099ce59b81df555c
George Cooper <coopsgeorge@gmail.com>
2020-03-05 12:07:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,6 @@ def naming_system
          sub_category: { label: "Order",
             sub_category: { label: "Family",
                sub_category: { label: "Genus"
-                 sub_category: { label: "Species"
-                 }}}}}}}
+                 }}}}}}
 
 end
